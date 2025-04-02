5ce0bf7fa5316ba0d2ac7ec49e9d265c56a62276
Jörg Aderhold <joerg@jaderbass.de>
2025-04-02 11:19:00 +0200

    add and run migrations

--- resources/views/u2-portfolio.blade.php
+++ resources/views/u2-portfolio.blade.php
@@ -2,6 +2,9 @@
   title={{$title}} 
   cardheader="Das machen wir alles:"
   cardtitle={{$heading}}
-  cardfooter="Weiterlesen...">
-  {!!$content!!}
+  cardfooter="Weiterlesen..."
+>
+  
+  {{$content}}
+
 </x-u2-master>
